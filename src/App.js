9a9ec50652480e082cd61ca822d272d4a5b692c7
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-22 03:50:16 +0600

    Login , signUp page desin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
+import Login from './Pages/Auth/Login/Login';
+import Signup from './Pages/Auth/Signup/Signup';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
 import MyProfile from './Pages/MyProfile/MyProfile';
@@ -13,10 +16,12 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='myProfile' element={<MyProfile/>}/>
-        <Route path='blogs' element={<Blogs/>}/>
-        <Route path='login' element={<Login/>}/>
+        <Route path='/myProfile' element={<MyProfile/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signUp' element={<Signup/>}/>
       </Routes>
+      <ToastContainer/>
     </div>
   );
 }
